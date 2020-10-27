4279f0c958564a5fa59b64cfd9d365a35384bd9e
stefanseeger <stefanseeger@users.noreply.github.com>
2020-10-27 19:32:52 +0100

    Update test.js

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("https://stefanseeger.github.io/vue-test-ts/");
-    cy.contains("h1", "Welcome to Stefans Vue.js + TypeScript App");
+    cy.contains("h1", "Welcome to Stefan's Vue.js + TypeScript App");
   });
 });
