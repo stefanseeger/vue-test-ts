fd2780c8db33a046381fa2ece4318fb4c15b8b8d
Stefan Seeger <seega@gmx.com>
2020-10-27 14:01:01 +0100

    enable dist

---
+++ dist/precache-manifest.bbc90e6378c7edf79f90f023c179765c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08a560cf87194be1bb7",
+    "url": "/css/app.eccc88d6.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6ccc584cd8707065948ce1475c1e2f8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5ea3d769e6cacab730",
+    "url": "/js/about.fe960957.js"
+  },
+  {
+    "revision": "e08a560cf87194be1bb7",
+    "url": "/js/app.172297ae.js"
+  },
+  {
+    "revision": "d1f80ae2a35c46b4d596",
+    "url": "/js/chunk-vendors.60769280.js"
+  },
+  {
+    "revision": "b6bfab4eb9bba9fa4b6b0d55a79b412e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
