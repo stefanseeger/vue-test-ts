55c1f0b896379ee7e8d9e274874e807baa179143
Stefan Seeger <seega@gmx.com>
2020-10-27 15:10:08 +0100

    customize

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,6 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("https://stefanseeger.github.io/vue-test-ts/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("h1", "Welcome to Stefans Vue.js + TypeScript App");
   });
 });
