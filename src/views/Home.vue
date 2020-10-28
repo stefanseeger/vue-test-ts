f9fde15aedd780363d11576026bd0a09033dde9a
stefanseeger <stefanseeger@users.noreply.github.com>
2020-10-28 09:14:37 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Stefan's Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome to GitHub Pages Vue.js + TypeScript App example" />
   </div>
 </template>
 
