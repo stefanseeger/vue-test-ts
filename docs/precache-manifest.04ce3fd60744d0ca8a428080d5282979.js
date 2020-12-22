3193a0163d4fef6fcc8357a6c1355081d0c9068a
Stefan Seeger <seega@gmx.com>
2020-12-22 00:06:19 +0000

    Replace docs with latests dist

---
+++ docs/precache-manifest.04ce3fd60744d0ca8a428080d5282979.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1461338a61f575afdd03",
+    "url": "/vue-test-ts/css/app.4ed96f2a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-test-ts/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a64c1549a2a9636f7f5f54bf20fda7fa",
+    "url": "/vue-test-ts/index.html"
+  },
+  {
+    "revision": "3f12c1a21b308e9a768d",
+    "url": "/vue-test-ts/js/about.56ec0140.js"
+  },
+  {
+    "revision": "1461338a61f575afdd03",
+    "url": "/vue-test-ts/js/app.13a0be1f.js"
+  },
+  {
+    "revision": "961f8fd080bfe4671072",
+    "url": "/vue-test-ts/js/chunk-vendors.26aac065.js"
+  },
+  {
+    "revision": "35e0b3ce0123e0862714875c9fc5c9bd",
+    "url": "/vue-test-ts/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-test-ts/robots.txt"
+  }
+]);
